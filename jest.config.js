166d363f6b7af686ec6479d764fcb4cffd1f8ded
David Allen <david.allen@grafana.com>
2025-08-03 16:38:56 -0400

    include coverage reports in build while unit testing

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,24 @@ process.env.TZ = 'UTC';
 module.exports = {
   // Jest configuration provided by Grafana scaffolding
   ...require('./.config/jest.config'),
+  
+  // Coverage configuration
+  collectCoverage: true,
+  coverageReporters: ['text', 'html', 'lcov'],
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.{spec,test,stories}.{ts,tsx}',
+    '!src/**/types.ts',
+    '!src/**/index.ts',
+    '!src/**/*.d.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
 };
