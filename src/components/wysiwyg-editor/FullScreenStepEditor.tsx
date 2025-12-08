ee2e43bd34972ea5b9735de88f1f6161beb84926
Jack Baldry <jack.baldry@grafana.com>
2025-12-08 13:26:48 +0000

    Prettier
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- src/components/wysiwyg-editor/FullScreenStepEditor.tsx
+++ src/components/wysiwyg-editor/FullScreenStepEditor.tsx
@@ -876,7 +876,11 @@ export function FullScreenStepEditor({
           <div className={styles.warningBox}>
             {hasWarnings && (
               <Alert title="Selector warnings" severity="warning">
-                <ul>{pendingClick?.warnings?.map((warning, idx) => <li key={idx}>{warning}</li>)}</ul>
+                <ul>
+                  {pendingClick?.warnings?.map((warning, idx) => (
+                    <li key={idx}>{warning}</li>
+                  ))}
+                </ul>
               </Alert>
             )}
             {isNonUnique && (
